b422e182770e8939f75fb850109ee703d3014116
Daniel <dpalencia2112@gmail.com>
2019-03-04 14:27:12 -0800

    Implementing huffman decoding

--- Adv. Data Structures/HW3/main.cpp
+++ Adv. Data Structures/HW3/main.cpp
@@ -9,11 +9,18 @@
 #include <iomanip>
 using namespace std;
 void die(const string & e); // Die helper function
+string getBuffer(const string & inputFile); // This helper function will read the input file and return a "buffer" that contains the contents of the text
+void getFreq(unsigned long long freq[256], const string & buffer); // Read from buffer and change freq so that freq[n] = freq of char with ascii code N
+void deallocate(Node* p); // Helper function to deallocate the nodes in the tree from DMA in huffman function
+void compressWrite(const string code[256], const string & buffer, const string & outputFile); // Reads the input file and writes the code to the output file
+void decode(const string & inputFile, const string & outputFile, const Node* tree);
+ifstream ifOpenBin(const string & fileName); // Opens input files in binary mode
+ofstream ofOpenBin(const string & fileName); // Opens output files in binary mode
 class Node {
 public:
 	Node * _left;
 	Node* _right;
-	char _c;
+	unsigned char _c;
 	unsigned long long _freq;
 	Node(char c = '0', unsigned long long freq = 0, Node* left = nullptr, Node* right = nullptr) :
 		_c(c), _freq(freq), _left(left), _right(right) {};
@@ -22,6 +29,27 @@ public:
 		return n1->_freq > n2->_freq;
 	}
 };
+void traverse(const Node* p, ofstream & o, const string & buffer); // Traverse except this time we return the char contained in the leaf for decoding purposes
+void traverse(string code[256], Node* p); // Recursive function to traverse huffman tree and encode
+void traverse(string code[256], Node* p, string temp, char b);
+Node* huffman(string code[256], const unsigned long long freq[256]);
+int main() {
+	string code[256], inputFile, littleOut, bigOut, buffer;
+	unsigned long long f[256];
+	cout << "Enter the name of your input file." << endl;
+	getline(cin, inputFile);
+	cout << "Enter the name of the file to which we will write the compressed input file. " << endl;
+	getline(cin, littleOut);
+	cout << "Enter the name of the file to which we will write the decoded compressed file. " << endl;
+	getline(cin, bigOut);
+	buffer = getBuffer(inputFile);
+	getFreq(f, buffer); // Get the frequency of each char and write it to our "f" array.
+	Node* huff = huffman(code, f); // Build the huffman code from our freq values and store it in the code string
+	compressWrite(code, buffer, littleOut); // Read from the buffer and write the huffman encoded chars to the "little" output file.
+	decode(littleOut, bigOut, huff); // Decode the compressed file and write it to the big out file
+	system("pause");
+	return 0;
+}
 
 Node* Node::newNode(char c, unsigned long long freq) { // Create a new node and return a pointer to it
 	Node* p = nullptr;
@@ -34,33 +62,6 @@ Node* Node::newNode(char c, unsigned long long freq) { // Create a new node and 
 	return p;
 };
 
-string getBuffer(const string & inputFile); // This helper function will read the input file and return a "buffer" that contains the contents of the text
-void getFreq(unsigned long long freq[256], const string & buffer); // Read from buffer and change freq so that freq[n] = freq of char with ascii code N
-void huffman(string code[256], const unsigned long long freq[256]);
-void traverse(string code[256], Node* p); // Recursive function to traverse huffman tree
-void traverse(string code[256], Node* p, string temp, char b);
-void deallocate(Node* p); // Helper function to deallocate the nodes in the tree from DMA in huffman function
-void compressWrite(const string code[256], const string & buffer, const string & outputFile); // Reads the input file and writes the code to the output file
-ifstream ifOpenBin(const string & fileName); // Opens input files in binary mode
-ofstream ofOpenBin(const string & fileName); // Opens output files in binary mode
-
-int main() {
-	string code[256], inputFile, littleOut, bigOut, buffer;
-	unsigned long long f[256];	
-	cout << "Enter the name of your input file." << endl;
-	getline(cin, inputFile);
-	cout << "Enter the name of the file to which we will write the compressed input file. " << endl;
-	getline(cin, littleOut);
-	cout << "Enter the name of the file to which we will write the decoded compressed file. " << endl;
-	getline(cin, bigOut);
-	buffer = getBuffer(inputFile);
-	getFreq(f, buffer); // Get the frequency of each char and write it to our "f" array.
-	huffman(code, f); // Build the huffman code from our freq values and store it in the code string
-	compressWrite(code, buffer, littleOut); // Read from the input file and write the huffman encoded chars to the "little" output file.
-	system("pause");
-	return 0;
-}
-
 void getFreq(unsigned long long freq[256], const string & buffer) {
 	for (unsigned i = 0; i < 256; i++) { // Initialize freq array
 		freq[i] = 0;
@@ -68,12 +69,12 @@ void getFreq(unsigned long long freq[256], const string & buffer) {
 	for (unsigned i = 0; i < buffer.size(); i++) { // Read through buffer string and increment freq[i] where is i is the hex value of the char
 		if (buffer.at(i) < 0) // Handle the case where the char is < 0 to get appropriate array index for ASCII value
 			freq[buffer.at(i) + 256]++;
-		else 
+		else
 			freq[buffer.at(i)]++;
 	}
 }
 
-void huffman(string code[256], const unsigned long long freq[256]) {
+Node* huffman(string code[256], const unsigned long long freq[256]) {
 	// Use the priority queue to push and pop nodes
 	// Using a vector of pointers to nodes so that I can dynamically allocate nodes,
 	// and assign these new pointers to my nodes
@@ -104,7 +105,7 @@ void huffman(string code[256], const unsigned long long freq[256]) {
 		if (freq[i + 256] > 0)
 			cout << setw(9) << i + 256 << setw(11) << freq[i + 256] << setw(40) << code[i + 256] << endl;
 	}
-	deallocate(root);
+	return root;
 }
 
 void traverse(string code[256], Node* p) {
@@ -139,6 +140,32 @@ void traverse(string code[256], Node* p, string temp, char b) {
 	code[p->_c] = temp;
 }
 
+void traverse(const Node* p, ofstream & o, const string & buffer) { // Recursively traverse tree and write to the output file
+	static unsigned bitsRemaining = 8;
+	static unsigned i = 0;
+
+	if (p->_left == nullptr && p->_right == nullptr) {
+		o.put(p->_c);
+	}
+
+	if (p->_left != nullptr && p->_right != nullptr) {
+		if (bitsRemaining == 0) {
+			bitsRemaining = 8;
+			i++;
+		}
+
+		if ((buffer[i] >> (bitsRemaining - 1)) & 1) {// If the leftmost bit is 1, traverse right
+			bitsRemaining--;
+			traverse(p->_right, o, buffer);
+		}
+		else {
+			bitsRemaining--;
+			traverse(p->_left, o, buffer); // If the leftmost bit is 0, traverse left
+		}
+	}
+}
+
+
 void deallocate(Node* p) {	// Recursive memory deallocation
 	if (p->_left != nullptr && p->_right != nullptr) {
 		deallocate(p->_left);
@@ -147,17 +174,61 @@ void deallocate(Node* p) {	// Recursive memory deallocation
 	delete p;
 }
 
+
 void compressWrite(const string code[256], const string & buffer, const string & outputFile) { // Reads the input file and writes the code to the "little" output file
 	ofstream out = ofOpenBin(outputFile);
-	for (unsigned i = 0; i < buffer.size(); i++) // Iterate through buffer and write the code for eachc corresponding char to the output file.
+	string carr; // This will hold the huffman-encoded counterpart of the buffer
+	for (unsigned i = 0; i < buffer.size(); i++) { // Iterate through buffer and store the huffman encoding into a string
 		if (buffer.at(i) < 0)
-			out << code[buffer.at(i) + 256];
+			carr += code[buffer.at(i) + 256];
 		else
-			out << code[buffer.at(i)];
+			carr += code[buffer.at(i)];
+	}
+	// Write the number of chars in the output file as an unsigned long long
+	unsigned long long charCount = buffer.size(); // Get the char count 
+	unsigned long long* countPtr = &charCount; // We need a pointer to our char count to pass it to memcpy
+	char lengthBytes[8]; // Pointer to a fixed block of 8 char bytes
+	memcpy(lengthBytes, countPtr, sizeof(*countPtr)); // Copy the memory in countPtr to lengthBytes ptr
+	out.write(lengthBytes, 8);
+	unsigned remainingBits = 8; // Count how many bits we have as we write bits to the byte variable
+	unsigned byte = 0; // This will be the byte variable we will bit manipulate
+	for (unsigned i = 0; i < carr.size(); i++) {
+		if (remainingBits == 0) {
+			out.put(byte);
+			byte = 0; // Flush the byte
+			remainingBits = 8; // Reset remaining bits count
+		}
+		byte <<= 1;
+		if (carr.at(i) == '1')
+			byte |= 1; // Put the current bit into our byte
+		remainingBits--;
+	}
 	out.close();
-} 
+}
 
-string getBuffer(const string & inputFile) {
+void decode(const string & inputFile, const string & outputFile, const Node* tree) {
+	ifstream in = ifOpenBin(inputFile);
+	ofstream out = ofOpenBin(outputFile);
+	string buffer; // This string will store each byte in the input file
+	char lengthBytes[8]; // Read the first 8 bytes of the file and get its length. Needs to be pointer so we can use the memcpy function.
+	in.read(lengthBytes, 8);
+	unsigned long long charCount;
+	unsigned long long* countPtr = &charCount;
+	memcpy(countPtr, lengthBytes, sizeof(lengthBytes));
+	char c;
+	while (in.get(c)) {
+		buffer += c;
+	}
+	if (!in.eof())
+		die("there was an error reading the file.");
+	for (unsigned i = 0; i < charCount; i++) {
+		traverse(tree, out, buffer);
+	}
+	in.close();
+	out.close();
+}
+
+string getBuffer(const string & inputFile) { // Read a file and get the string buffer for input
 	string buffer;
 	char c;
 	ifstream in = ifOpenBin(inputFile);
